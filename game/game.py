b67d5a4ff54d3b761aae3add404cb976da37c2a4
Fefan <czmikton2020@gmail.com>
2026-03-25 18:18:19 +0100

    fix all of jaro's mistakes (there were many)

--- game/game.py
+++ game/game.py
@@ -5,21 +5,26 @@ pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True
-framerate = 60
-maxX, maxY = pygame.display.get_surface().get_size()
-print(maxX, maxY)
+frame_rate = 60
+max_x, max_y = pygame.display.get_surface().get_size()
+scale = 20  # scales the size of everything
+print(max_x, max_y)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        image = pygame.image.load("main/game/assets/placeholder.png")
-        self.rect = image.get_rect()
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("assets/placeholder.png")
+        self.image = pygame.transform.scale_by(self.image, scale)
+        self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, x, y, img, breakable, moving):
-        image = pygame.image.load(img)
-        self.rect = image.get_rect()
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(img)
+        self.image = pygame.transform.scale_by(self.image, scale)
+        self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.breakable = breakable
@@ -27,14 +32,16 @@ class Block(pygame.sprite.Sprite):
 
 class Ground(pygame.sprite.Sprite):
     def __init__(self, x , y):
-        self.image = pygame.Surface((maxX, 10))
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((max_x, 10))
+        self.image = pygame.transform.scale_by(self.image, scale)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         
         
-ground = Ground(0, maxY)
-frog = Player(maxX/2, maxY)
+ground = Ground(0, max_y)
+frog = Player(max_x / 2 , max_y / 2)
 main_group = pygame.sprite.Group(ground, frog)
 
 while running:
@@ -45,11 +52,12 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
-    main_group.draw()
+    screen.fill("#101010")
 
-    screen.fill("black")
+    main_group.draw(screen)
+
     pygame.display.flip()
-    dt = clock.tick(framerate)
+    dt = clock.tick(frame_rate) / 1000
 
 pygame.quit()
 
