cd2f22a24cc273f11f4a002cbfefcd5bb2785c89
jaro51 <roskot.j@proton.me>
2026-03-25 13:11:34 +0100

    update game.py

--- main/game/game.py
+++ main/game/game.py
@@ -1,11 +1,28 @@
 import pygame
 
-class Object(pygame.sprite.Sprite):
-    def __init__(self, moving, time, x, y, image):
-        self.rect = pygame.image.load(image).get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.moving = moving
-        self.time = time
+pygame.init()
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+running = True
+framerate = 60
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+    screen.fill("black")
+    pygame.display.flip()
+    dt = clock.tick(framerate)
+
+
+
+
+
+pygame.quit()
 
 
